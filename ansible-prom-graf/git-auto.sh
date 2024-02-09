e1a51c15cb6c535c6e66b661b76cee55a4a22e2b
Aby Varghese <abyvarghese2007@gmail.com>
2024-02-08 23:39:18 -0600

    hey

---
+++ ansible-prom-graf/git-auto.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Navigate to your Git repository directory
+# cd /path/to/your/repository
+
+# Add all changes to staging
+git add .
+
+# Commit the changes with a message
+git commit -m "hey"
+
+# Push the changes to the master branch on origin
+git push origin master
+
